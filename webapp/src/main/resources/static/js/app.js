d91a9d03fbcb4f3559bd4526077a618e421cec1f
Karsten Röthig <karstenroethig@users.noreply.github.com>
2019-11-11 17:00:02 +0100

    Switch to thymeleaf fragment expressions for layouts

--- webapp/src/main/resources/static/js/app.js
+++ webapp/src/main/resources/static/js/app.js
@@ -1,6 +1,8 @@
 
 $( document ).ready( function() {
 
+	$('.select2-multiple').select2();
+
 	// delete modals: transfer the id to the modal form
 	$( '#deleteModal' ).on( 'show.bs.modal', function( event ) {
 		var button = $( event.relatedTarget ); // Button that triggered the modal
